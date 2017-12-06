d30529d5ff45f2083e0372c04973dd577c34ade8
Vit Benton <viteksk8alot@gmail.com>
2017-12-05 20:46:30 -0500

    added chord images, added to about page

--- client/src/components/About/Body.js
+++ client/src/components/About/Body.js
@@ -6,7 +6,27 @@ const Body = props =>
 
   <div>
       <div className="container">
-        <h2>About Page!</h2>
+         <h2>About The Guitar Key</h2>
+          <p>With little to no focus on music theory, The Guitar Key will drive users to learn how to play songs and how to play in key.</p>
+          <p>Most guitar playing is improvisation and self-taught. The basic philosophy behind The Guitar Key parallels conventional learning methods and should better assist with guitar playing development.</p>
+          <h3>Technologies Used</h3>
+          
+          <ul>
+            
+            <li>ReactJS</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>Express-Sessions</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
+            <li>Passport.JS</li>
+            <li>Bcrytp</li>
+            <li>Bootstrap</li>
+            <li>Axios</li>
+            <li>Spotify API</li>
+            <li>“ultimate-guitar-scraper” node module</li>
+
+          </ul>
       </div>
   
     {!props.user._id //if _id doesn't exist AKA user is not logged in, insert LoginModal and Register Modal
--- client/src/components/Home/Jumbotron.js
+++ client/src/components/Home/Jumbotron.js
@@ -7,8 +7,8 @@ const Jumbotron = () =>
     <div className="container">
         <div className="jumbotron">
             <h1>The Guitar Key</h1>
-            <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-            <p>To see the difference between static and fixed top navbars, just scroll.</p>
+            <p>With little to no focus on music theory, The Guitar Key will drive users to learn how to play songs and how to play in key.</p>
+            <p>Most guitar playing is improvisation and self-taught. The basic philosophy behind The Guitar Key parallels conventional learning methods and should better assist with guitar playing development.</p>
             <p>
                 <Link className="btn btn-lg btn-success" to="/about" role="button">Learn More</Link>
             </p>
